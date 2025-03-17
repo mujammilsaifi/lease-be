b1797d4ccceaeb70fab321128f1888ef0f4298c7
Mujammil <mujammilkhan00738@gmail.com>
2025-03-17 18:02:04 +0530

    rentAdhocWise add

--- src/models/lease.model.ts
+++ src/models/lease.model.ts
@@ -21,6 +21,11 @@ interface ILease extends Document {
     dateRange: string[];
     rate: number;
   }[];
+  natureOfRent: "adhoc-type" | "systematic";
+  rentAdhocWise?: {
+    date: string;
+    rent: number;
+  }[];
   systematicEscalations?: {
     dateRange: string;
     frequency: string;
@@ -38,53 +43,98 @@ interface ILease extends Document {
 }
 
 // Mongoose Schema
-const LeaseSchema: Schema = new Schema({
-  lessorName: { type: String, required: true },
-  natureOfLease: { type: String, required: true },
-  leaseClosureDate: { type: String, required: false},
-  remarks: { type: String, required: false},
-  status: {
-    type: String,
-    enum: ["active", "close"], 
-    default: "active",
-    required: false
+const LeaseSchema: Schema = new Schema(
+  {
+    lessorName: { type: String, required: true },
+    natureOfLease: { type: String, required: true },
+    leaseClosureDate: { type: String, required: false },
+    remarks: { type: String, required: false },
+    status: {
+      type: String,
+      enum: ["active", "close"],
+      default: "active",
+      required: false,
+    },
+    period: { type: String, required: true },
+    agreementCode: { type: String, required: true, unique: true },
+    leasePeriod: { type: [String], required: true },
+    lockingPeriod: { type: [String], required: true },
+    leaseWorkingPeriod: { type: [String], required: true },
+    rentPaymentType: {
+      type: String,
+      required: function (this: ILease) {
+        return this.natureOfRent === "systematic";
+      },
+    },
+    rentPaymentFrequency: {
+      type: String,
+      required: function (this: ILease) {
+        return this.natureOfRent === "systematic";
+      },
+    },
+    rentAmount: {
+      type: Number,
+      required: function (this: ILease) {
+        return this.natureOfRent === "systematic";
+      },
+    },
+    rentPaymentDate: {
+      type: Number,
+      required: function (this: ILease) {
+        return this.natureOfRent === "systematic";
+      },
+    },
+    securityDeposit: { type: Number, required: false },
+    discountingRates: [
+      {
+        dateRange: { type: [String], required: true },
+        rate: { type: Number, required: true },
+      },
+    ],
+    natureOfRent: {
+      type: String,
+      enum: ["adhoc-type", "systematic"],
+      required: true,
+    },
+    rentAdhocWise: [
+      {
+        date: {
+          type: String,
+          required: function (this: ILease) {
+            return this.natureOfRent === "adhoc-type";
+          },
+        },
+        rent: {
+          type: Number,
+          required: function (this: ILease) {
+            return this.natureOfRent === "adhoc-type";
+          },
+        },
+      },
+    ],
+    systematicEscalations: [
+      {
+        dateRange: { type: String, required: true },
+        frequency: { type: String, required: true },
+        percentage: { type: Number, required: true },
+      },
+    ],
+    adhocEscalations: [
+      {
+        dateRange: { type: [String], required: true },
+        frequency: { type: String, required: true },
+        amount: { type: Number, required: true },
+      },
+    ],
+    rentFreePeriods: [
+      {
+        dateRange: { type: [String], required: true },
+        percentage: { type: Number, required: true },
+      },
+    ],
   },
-  period: { type:String, required: true },
-  agreementCode: { type: String, required: true, unique: true },
-  leasePeriod: { type: [String], required: true },
-  lockingPeriod: { type: [String], required: true },
-  leaseWorkingPeriod: { type: [String], required: true },
-  rentPaymentType: { type: String, required: true },
-  rentPaymentFrequency: { type: String, required: true },
-  rentAmount: { type: Number, required: true },
-  rentPaymentDate: { type: Number, required: true },
-  securityDeposit: { type: Number, required: false }, 
-  discountingRates: [
-    {
-      dateRange: { type: [String], required: true },
-      rate: { type: Number, required: true },
-    },
-  ],
-  systematicEscalations: [
-    {
-      dateRange: { type: String, required: true },
-      frequency: { type: String, required: true },
-      percentage: { type: Number, required: true },
-    },
-  ],
-  adhocEscalations: [
-    {
-      dateRange: { type: [String], required: true },
-      frequency: { type: String, required: true },
-      amount: { type: Number, required: true },
-    },
-  ],
-  rentFreePeriods: [
-    {
-      dateRange: { type: [String], required: true },
-      percentage: { type: Number, required: true },
-    },
-  ],
-},{timestamps:true});
+  { timestamps: true }
+);
+
 
 export default mongoose.model<ILease>("Lease", LeaseSchema);
